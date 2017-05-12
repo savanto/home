ae52bf133bdd8d5fb9b8758de90f6385168644d8
savanto <savanto@users.noreply.github.com>
2017-05-12 06:01:40 -0700

    Remove openbox & terminator configs

--- openbox/.config/openbox/onwatch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Pipe menu wrapper for onwatch script for openbox
-#
-
-echo "<openbox_pipe_menu>"
-echo "  <item label=\"$(onwatch -bnw)\" />"
-echo "</openbox_pipe_menu>"
--- openbox/.config/openbox/time.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "<openbox_pipe_menu>"
-echo "  <item label=\"$(date)\" />"
-echo "</openbox_pipe_menu>"
